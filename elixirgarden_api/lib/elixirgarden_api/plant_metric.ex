87bab58919235788360dadc4c6dd991bb4fc2bb4
levfurtado <levfurtado@gmail.com>
2016-07-09 01:44:37 -0400

    able to make repo

--- elixirgarden_api/lib/elixirgarden_api/plant_metric.ex
+++ elixirgarden_api/lib/elixirgarden_api/plant_metric.ex
@@ -2,9 +2,9 @@ defmodule PlantMetric do
   use Ecto.Schema
 
   schema "plant_metrics" do
-    field :soil_temp
-    field :soil_humidity
-    field :water_valve_status
+    field :soil_temp, :float, default: 0.0
+    field :soil_humidity, :float, default: 0.0
+    field :water_valve_status, :boolean
 
     timestamps
   end
--- elixirgarden_api/priv/repo/migrations/20160709043459_create_plant_metrics.exs
+++ elixirgarden_api/priv/repo/migrations/20160709043459_create_plant_metrics.exs
@@ -3,8 +3,8 @@ defmodule ElixirgardenApi.Repo.Migrations.CreatePlantMetrics do
 
   def change do
     create table(:plant_metrics) do
-      add :soil_temp, :float
-      add :soil_humidity, :float
+      add :soil_temp, :float, default: 0.0
+      add :soil_humidity, :float, default: 0.0
       add :water_valve_status, :boolean
 
       timestamps
