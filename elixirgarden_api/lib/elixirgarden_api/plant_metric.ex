1d85aad8f5391b7f6876edc3274b408c801d2d6e
levfurtado <levfurtado@gmail.com>
2016-07-10 15:19:46 -0400

    added changesets

--- elixirgarden_api/lib/elixirgarden_api/plant_metric.ex
+++ elixirgarden_api/lib/elixirgarden_api/plant_metric.ex
@@ -1,11 +1,18 @@
 defmodule PlantMetric do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "plant_metrics" do
     field :soil_temp, :float, default: 0.0
     field :soil_humidity, :float, default: 0.0
     field :water_valve_status, :boolean
-
     timestamps
   end
+
+  def changeset(plant_metric, params \\ %{}) do
+    plant_metric
+    |> cast(params, [:soil_temp, :soil_humidity, :water_valve_status])
+    |> validate_required([:soil_temp, :soil_humidity, :water_valve_status])
+  end
+
 end
