e9e1bede6c52234dd6db7cba094c55837f941fe7
levfurtado <levfurtado@gmail.com>
2016-07-10 20:28:18 -0400

    can make queries

--- elixirgarden_api/lib/elixirgarden_api/plant_metric.ex
+++ elixirgarden_api/lib/elixirgarden_api/plant_metric.ex
@@ -6,12 +6,13 @@ defmodule PlantMetric do
     field :soil_temp, :float, default: 0.0
     field :soil_humidity, :float, default: 0.0
     field :water_valve_status, :boolean
+    field :position, {:array, :string}
     timestamps
   end
 
   def changeset(plant_metric, params \\ %{}) do
     plant_metric
-    |> cast(params, [:soil_temp, :soil_humidity, :water_valve_status])
+    |> cast(params, [:soil_temp, :soil_humidity, :water_valve_status, :position])
     |> validate_required([:soil_temp, :soil_humidity, :water_valve_status])
   end
 
--- elixirgarden_api/priv/repo/migrations/20160709043459_create_plant_metrics.exs
+++ elixirgarden_api/priv/repo/migrations/20160709043459_create_plant_metrics.exs
@@ -6,7 +6,7 @@ defmodule ElixirgardenApi.Repo.Migrations.CreatePlantMetrics do
       add :soil_temp, :float, default: 0.0
       add :soil_humidity, :float, default: 0.0
       add :water_valve_status, :boolean
-
+      add :position, {:array, :string}
       timestamps
     end
   end
